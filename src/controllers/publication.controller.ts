652af6aa75d368ad49d4187ff5f843d24240544f
Kevin Coffey <kevdougful@gmail.com>
2018-07-10 12:56:01 -0500

    Add Remaining HTTP Verbs
    This adds routes for PUT, PATCH, and DELETE as well as a "findById" GET route.

--- src/controllers/publication.controller.ts
+++ src/controllers/publication.controller.ts
@@ -9,7 +9,7 @@ import {
   get,
   put,
   patch,
-  del,
+  del
 } from '@loopback/rest'
 
 export class PublicationController {
@@ -22,11 +22,42 @@ export class PublicationController {
     return await this.repo.find()
   }
 
+  @get('/publications/{pmid}')
+  async findPublicationByPmid(
+    @param.path.number('pmid') pmid: number
+  ): Promise<Publication> {
+    return await this.repo.findById(pmid)
+  }
+
   @post('/publications')
-  async createPublication(@requestBody() publication: Publication) {
+  async createPublication(
+    @requestBody() publication: Publication
+  ): Promise<Publication> {
     if (!publication.title) {
       throw new HttpErrors.BadRequest('title is required')
     }
     return await this.repo.create(publication)
+  }
+
+  @put('/publications')
+  async updatePublication(
+    @requestBody() publication: Publication
+  ): Promise<boolean> {
+    return await this.repo.update(publication)
+  }
+
+  @patch('/publications/{pmid}')
+  async updatePublicationByPmid(
+    @param.path.number('pmid') pmid: number,
+    @requestBody() publication: Publication
+  ): Promise<boolean> {
+    return await this.repo.updateById(pmid, publication)
+  }
+
+  @del('/publications/{pmid}')
+  async deletePublicationByPmid(
+    @param.path.number('pmid') pmid: number
+  ): Promise<boolean> {
+    return await this.repo.deleteById(pmid)
   }
 }
